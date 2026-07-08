9c7028d5dc4b01616953c1554ccfcef014af07be
Ben-DataPaws <167247129+Ben-DataPaws@users.noreply.github.com>
2026-07-08 15:13:24 -0400

    Update description

--- sp_DBPermissions.sql
+++ sp_DBPermissions.sql
@@ -33,24 +33,22 @@ from a role or adding them to a role.
 
 Parameters:
     @DBName
-        If NULL use the current database, otherwise give permissions based on the parameter.
+        If NULL use the current database, otherwise show permissions for the specified database.
     
-        There is a special case where you pass in ALL to the @DBName.  In this case the SP
-        will loop through (yes I'm using a cursor) all of the DBs in sysdatabases and run
-        the queries into temp tables before returning the results.  WARNINGS: If you use
-        this option and have a large number of databases it will be SLOW.  If you use this
-        option and don't specify any other parameters (say a specific @Principal) and have
-        even a medium number of databases it will be SLOW.  Also the undo/do scripts do 
-        not have USE statements in them so please take that into account.
+        Pass in "ALL" to @DBName and it will loop through all databases.
+		WARNINGS: If you use this option and have a large number of databases it will be SLOW.
+		If you use this option and don't specify any other parameters (say a specific @Principal)
+		and have even a medium number of databases it will be SLOW. Also the undo/do scripts do not
+		have USE statements in them so please take that into account.
     @Principal
         If NOT NULL then all three queries only pull for that database principal.  @Principal
-        is a pattern check.  The queries check for any row where the passed in value exists.
+        is a pattern check. The queries check for any row where the passed in value exists.
         It uses the pattern '%' + @Principal + '%'
     @Role
-        If NOT NULL then the roles query will pull members of the role.  If it is NOT NULL and
+        If NOT NULL then the roles query will pull members of the role. If it is NOT NULL and
         @DBName is NULL then DB principal and permissions query will pull the principal row for
-        the role and the permissions for the role.  @Role is a pattern check.  The queries 
-        check for any row where the passed in value exists.  It uses the pattern '%' + @Role +
+        the role and the permissions for the role. @Role is a pattern check. The queries 
+        check for any row where the passed in value exists. It uses the pattern '%' + @Role +
         '%'
     @Type
         If NOT NULL then all three queries will only pull principals of that type.  
@@ -63,62 +61,46 @@ Parameters:
     @ObjectName
         If NOT NULL then the third query will display permissions specific to the object 
         specified and the first two queries will display only those users with those specific
-        permissions.  Unfortunately at this point only objects in sys.all_objects will work.
-        This parameter uses the pattern '%' + @ObjectName + '%'
+        permissions. This parameter uses the pattern '%' + @ObjectName + '%'
     @Permission
         If NOT NULL then the third query will display only permissions that match what is in
-        the parameter.  The first two queries will display only those users with that specific
+        the parameter. The first two queries will display only those users with that specific
         permission.
     @LoginName
         If NOT NULL then each of the queries will only pull back database principals that
         have the same SID as a login that matches the pattern '%' + @LoginName + '%'
-    @UseLikeSearch
-        When this is set to 1 (the default) then the search parameters will use LIKE (and 
-        %'s will be added around the @Principal, @Role, @ObjectName, and @LoginName parameters).  
+	@UseLikeSearch
+		When this is set to 1 (the default) then the search parameters will use LIKE (and 
+		%'s will be added around the @Principal, @Role, @ObjectName, and @LoginName parameters).  
         When set to 0 searchs will use =.
-    @IncludeMSShipped
-        When this is set to 1 (the default) then all principals will be included.  When set 
-        to 0 the fixed server roles and SA and Public principals will be excluded.
-    @CopyTo
-        If @Principal is filled in then the value in @CopyTo is used in the drop and create
-        scripts instead of @Principal. In the case of the CREATE USER statement @CopyTo 
-        also replaces the name of the server level principal, however it does not affect the
-        default schema name.
-        NOTE: It is very important to note that if @CopyTo is not a valid name the drop/create
-        scripts may fail.
-    @DropTempTables
-        When this is set to 1 (the default) the temp tables used are dropped.  If it's 0
-        then the temp tables are kept for references after the code has finished.
-        The temp tables are:
-            #DBPrincipals
-            #DBRoles 
-            #DBPermissions
-    @ShowOrphans
-        By default this is 0. If it is 1 then it shows only orphaned principals and scripts to fix them.
-                  Note: This option is 2012 and up only.
-    @Output
-        What type of output is desired.
-        Default - Either 'Default' or it doesn't match any of the allowed values then the SP
-                    will return the standard 3 outputs.
-        None - No output at all.  Usually used if you keeping the temp tables to do your own
-                    reporting.
-        CreateOnly - Only return the create scripts where they aren't NULL.
-        DropOnly - Only return the drop scripts where they aren't NULL.
-        ScriptOnly - Return drop and create scripts where they aren't NULL.
-        Report - Returns one output with one row per principal and a comma delimited list of
-                    roles the principal is a member of and a comma delimited list of the 
-                    individual permissions they have.
+	@IncludeMSShipped
+		When this is set to 1 (the default) then all principals will be included. When set 
+		to 0 the fixed server roles and SA and Public principals will be excluded.
+	@DropTempTables
+		When this is set to 1 (the default) the temp tables used are dropped. If it's 0
+		then the temp tables are kept for references after the code has finished.
+		The temp tables are:
+			#DBPrincipals
+			#DBRoles 
+			#DBPermissions
+	@Output
+		Type of output desired
+		Default 	- Either 'Default' or it doesn't match any of the allowed values then the SP will return the standard 3 outputs.
+		None		- No output at all.  Usually used if you keeping the temp tables to do your own reporting.
+		CreateOnly 	- Returns scripts to create both the database principal and grant database permissions
+		DropOnly 	- Returns scripts to drop database permissions
+		ScriptsOnly - Returns drop and create scripts
+		Report 		- Returns one output with one row per principal and a comma delimited list of roles the principal is a member of and a comma delimited list of the individual permissions they have.
     @Print
         Defaults to 0, but if a 1 is passed in then the queries are not run but printed
-        out instead.  This is primarily for debugging.
+        out instead. This is primarily for debugging.
     
 Data is ordered as follows
     1st result set: DBPrincipal
     2nd result set: RoleName, UserName if the parameter @Role is used else
                     UserName, RoleName
-    3rd result set: If @ObjectName is used then DBName, SchemaName, ObjectName, Grantee_Name, permission_name
-                    otherwise DBName, GranteeName, SchemaName, ObjectName, permission_name
-
+    3rd result set: ObjectName then Grantee_Name if the parameter @ObjectName
+                    is used otherwise Grantee_Name, ObjectName
 */
 
 SET NOCOUNT ON
